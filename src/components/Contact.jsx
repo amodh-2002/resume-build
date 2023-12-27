85cb3f784b5cc166d8c086d0b84fe374e889f64a
unknown <snake@example.com>
2023-12-27 16:06:31 +0530

    added env file

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Contact = () => {
   return (
     <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
-        <form method='POST' action='https://getform.io/f/041a903f-2ccf-4155-b4ec-2397675e08a0' className='flex flex-col max-w-[600px] w-full'>
+        <form method='POST' action={import.meta.env.VITE_REACT_APP_FORM_LINK} className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
                 <p className='text-gray-300 py-4'>// Submit the form below or shoot me an email - amodhbh72@gmail.com</p>
