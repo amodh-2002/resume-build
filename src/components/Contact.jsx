1276b55046ce241c392633aeb5a8ef4c2c4eacab
unknown <snake@example.com>
2023-12-29 11:29:01 +0530

    changed in navbar home and contact.jsx again

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,9 +8,9 @@ const Contact = () => {
                 <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
                 <p className='text-gray-300 py-4'>// Submit the form below or shoot me an email - amodhbh72@gmail.com</p>
             </div>
-            <input className='bg-[#ccd6f6] p-2 rounded-md outline-none' type="text" placeholder='Name' name='name' />
-            <input className='my-4 p-2 bg-[#ccd6f6] rounded-md outline-none' type="email" placeholder='Email' name='email' />
-            <textarea className='bg-[#ccd6f6] p-2 rounded-md text-black outline-none' name="message" rows="10" placeholder='Message'></textarea>
+            <input required autoComplete='Name' className='bg-[#ccd6f6] p-2 rounded-md outline-none' type="text" placeholder='Name' name='name' />
+            <input required autoComplete='Email' className='my-4 p-2 bg-[#ccd6f6] rounded-md outline-none' type="email" placeholder='Email' name='email' />
+            <textarea required autoComplete='Message' className='bg-[#ccd6f6] p-2 rounded-md text-black outline-none' name="message" rows="10" placeholder='Message'></textarea>
             <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center rounded-md'>Let's Collaborate</button>
         </form>
     </div>
