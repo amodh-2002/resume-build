0713b07a0264b3da36099f547ba07100409333b3
Amodh <snake@example.com>
2024-10-24 12:56:05 +0530

    changes in contanct.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -42,7 +42,7 @@ const Contact = () => {
             Contact
           </p>
           <p className="text-gray-300 py-4">
-            // Submit the form below or shoot me an email - amodhbh72@gmail.com
+            // Submit the form below or shoot me an email
           </p>
         </div>
         <input
